6e09635d63d29b97806804704e771d0f7be5c93e
Satyam Sharma <satyamsharma49371@gmail.com>
2026-07-27 18:05:52 +0530

    Updated training dataset and model

--- train_model.py
+++ train_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import joblib
 data = pd.read_csv("comments.csv")
 
-print(data)
+print(data["label"].value_counts())
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
@@ -13,8 +13,16 @@ from sklearn.pipeline import Pipeline
 X = data["comment"]
 y = data["label"]
 model = Pipeline([
-    ("vectorizer", TfidfVectorizer()),
-    ("classifier", LogisticRegression(max_iter=1000))
+    ("vectorizer", TfidfVectorizer(
+        lowercase=True,
+        ngram_range=(1,2),
+        min_df=2
+    )),
+    ("classifier", LogisticRegression(
+        max_iter=2000,
+        class_weight="balanced",
+        random_state=42
+    ))
 ])
 
 model.fit(X, y)
